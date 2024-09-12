8eb1484e2a852d9433159a5a8ed877cea88fa7c3
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-09-12 05:21:58 -0700

    Add documentation for servos and pwm

--- src/pio_pwm.rs
+++ src/pio_pwm.rs
@@ -1,22 +1,27 @@
 use core::time::Duration;
 
 use embassy_rp::clocks::{self};
-use embassy_rp::pio::{self};
+use embassy_rp::pio::{self, Pin};
 use embassy_rp::gpio::Level;
 use ::pio::InstructionOperands;
 use pio::{
     Common, Config, Instance, PioPin, StateMachine,
 };
 
+// This converts the duration provided into the number of cycles the PIO needs to run to make it take the same time
 pub fn to_pio_cycles(duration: Duration) -> u32 {
     (clocks::clk_sys_freq() / 1_000_000) / 3 * duration.as_micros() as u32 // parentheses are required to prevent overflow
 }
 
+// Just a struct that stores the PIO state machine. Remember there are two PIO blocks, each with four state machines
+// We are just running one program, so we just need one state machine
 pub struct PwmPio<'d, T: Instance, const SM: usize> {
     sm: StateMachine<'d, T, SM>,
+    pin: Pin<'d, T>
 }
 
 impl<'d, T: Instance, const SM: usize> PwmPio<'d, T, SM> {
+    // Attach a servo to the specified pio, state machine, and pin.
     pub fn attach(
         pio: &mut Common<'d, T>,
         mut sm: StateMachine<'d, T, SM>,
@@ -46,17 +51,20 @@ impl<'d, T: Instance, const SM: usize> PwmPio<'d, T, SM> {
 
         sm.set_config(&cfg);
 
-        Self { sm }
+        Self { sm, pin }
     }
 
+    // Runs the servo input command, basically creating the PIO wave.
     pub fn start(&mut self) {
         self.sm.set_enable(true);
     }
 
+    // Stops the servo input command, ceasing all signals from the PIN that were generated via PIO.
     pub fn stop(&mut self) {
         self.sm.set_enable(false);
     }
 
+    // Set the duration of the period for the PWM wave, read more about pwm and servos here https://blog.wokwi.com/learn-servo-motor-using-wokwi-logic-analyzer/ 
     pub fn set_period(&mut self, duration: Duration) {
         let is_enabled = self.sm.is_enabled();
         while !self.sm.tx().empty() {}
@@ -71,13 +79,18 @@ impl<'d, T: Instance, const SM: usize> PwmPio<'d, T, SM> {
         }
     }
 
+    // Set the number of pio cycles to set the wave on high to.
     pub fn set_level(&mut self, level: u32) {
         self.sm.tx().push(level);
     }    
 
+    // Set the duration for the wave to be high on.
     pub fn write(&mut self, duration: Duration) {
         self.set_level(to_pio_cycles(duration));
     }
 
-
+    // Return the state machine and pin.
+    pub fn release(self) -> (StateMachine<'d, T, SM>, Pin<'d, T>) {
+        (self.sm, self.pin)
+    }
 }
--- src/servo.rs
+++ src/servo.rs
@@ -2,20 +2,25 @@ use core::time::Duration;
 use embassy_rp::pio::Instance;
 use crate::pio_pwm::PwmPio;
 
+// The following will be interpreted as micro seconds.
 const DEFAULT_MIN_PULSE_WIDTH: u64 = 1000; // uncalibrated default, the shortest duty cycle sent to a servo
 const DEFAULT_MAX_PULSE_WIDTH: u64 = 2000; // uncalibrated default, the longest duty cycle sent to a servo 
-const REFRESH_INTERVAL: u64 = 20000; // classic default period to refresh servos in microseconds 
+const REFRESH_INTERVAL: u64 = 20000; // classic default period to refresh servos in microseconds
 const DEFAULT_MAX_DEGREE_ROTATION: u64 = 180; // 180 degrees is typical
 
+// This struct stores the Servo's information, most notably the period, min_pulse_width, and max_pulse_width
+// To understand what each of these do, please read up on pwm, here https://blog.wokwi.com/learn-servo-motor-using-wokwi-logic-analyzer/
 pub struct ServoBuilder<'d, T: Instance, const SM: usize> {
     pwm: PwmPio<'d, T, SM>,
     period: Duration,
     min_pulse_width: Duration,
     max_pulse_width: Duration,
-    max_degree_rotation: u64,
+    max_degree_rotation: u64, // The max number of degrees a servo is able to rotate.
 }
 
 impl<'d, T: Instance, const SM: usize> ServoBuilder<'d, T, SM> {
+    // Create a new ServoBuilder, which is then used to instantiate a Servo struct. This is for configuration. Read up
+    // On Rust builder pattern here https://rust-unofficial.github.io/patterns/patterns/creational/builder.html
     pub fn new(pwm: PwmPio<'d, T, SM>) -> Self {
         Self {
             pwm,
@@ -26,26 +31,35 @@ impl<'d, T: Instance, const SM: usize> ServoBuilder<'d, T, SM> {
         }
     }
 
+    // Set the duration of the period that the servo uses, typically given in the datasheet.
+    // Default is 20 ms
     pub fn set_period(mut self, duration: Duration) -> Self {
         self.period = duration;
         self
     }
 
+    // Set the duration of the minimum pulse width that the servo uses, typically given in the datasheet.
+    // Default is 1 ms
     pub fn set_min_pulse_width(mut self, duration: Duration) -> Self {
         self.min_pulse_width = duration;
         self
     }
 
+    // Set the duration of the maximum pulse width that the servo uses, typically given in the datasheet.
+    // Default is 2 ms
     pub fn set_max_pulse_width(mut self, duration: Duration) -> Self {
         self.max_pulse_width = duration;
         self
     }
 
+    // Set the duration of the maximum number of degrees that the servo can rotate, typically given in the datasheet.
+    // Default is 180 degrees
     pub fn set_max_degree_rotation(mut self, degree: u64) -> Self {
         self.max_degree_rotation = degree;
         self
     }
 
+    // Get the Servo struct
     pub fn build(mut self) -> Servo<'d, T, SM> {
         self.pwm.set_period(self.period);
         Servo {
@@ -65,18 +79,22 @@ pub struct Servo<'d, T: Instance, const SM: usize> {
 }
 
 impl<'d, T: Instance, const SM: usize> Servo<'d, T, SM> {
+    // Start running the Servo
     pub fn start(&mut self) {
         self.pwm.start();
     }
 
+    // Stop the Servo from running
     pub fn stop(&mut self) {
         self.pwm.stop();
     }
 
+    // Write the duration that the Servo PWM cycle should run. More can be found from pio_pwm.rs
     pub fn write_time(&mut self, duration: Duration) {
         self.pwm.write(duration);
     }
 
+    // Rotate the servo to the specified degree.
     pub fn rotate(&mut self, degree: u64) {
         let degree_per_nano_second = (self.max_pulse_width.as_nanos() as u64 - self.min_pulse_width.as_nanos() as u64) / self.max_degree_rotation;
         let mut duration = Duration::from_nanos(degree * degree_per_nano_second + self.min_pulse_width.as_nanos() as u64);
