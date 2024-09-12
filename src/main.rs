fe8ceb986d17fd4ade47b94facce9538a11e55e3
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-09-12 04:32:21 -0700

    Added heap

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Debug;
 use core::time::Duration;
 
+extern crate alloc;
 use crate::pio_pwm::PwmPio;
-use embassy_rp::peripherals::{PIO0, USB};
+use alloc::vec;
 use embassy_executor::Spawner;
 use embassy_rp::adc::{Adc, Config as AdcConfig, InterruptHandler as AdcInterruptHandler};
 use embassy_rp::gpio;
 use embassy_rp::gpio::Pull;
+use embassy_rp::peripherals::{PIO0, USB};
 use embassy_rp::pio::{InterruptHandler as PioInterruptHandler, Pio};
 use embassy_rp::usb::{Driver, InterruptHandler as UsbInterruptHandler};
 use embassy_rp::{adc, bind_interrupts};
 use embassy_time::Timer;
 use gpio::{Level, Output};
+
+use embedded_alloc::Heap;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
+// use microflow::model;
 use sensor::{read_adc_value, CHANNEL_AMPLITUDES};
 
 use servo::ServoBuilder;
+
+use log::*;
 use usb::usb_task;
 use {defmt_rtt as _, panic_probe as _};
-use log::*;
 
 bind_interrupts!(struct Irqs {
     ADC_IRQ_FIFO => AdcInterruptHandler;
@@ -27,17 +38,25 @@ bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => PioInterruptHandler<PIO0>;
 });
 
+mod pio_pwm;
 mod sensor;
 mod servo;
 mod usb;
-mod pio_pwm;
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    // Initialize the allocator BEFORE you use it
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+    }
+
     let p = embassy_rp::init(Default::default());
     let driver = Driver::new(p.USB, Irqs);
     spawner.spawn(usb_task(driver)).unwrap();
-    Timer::after_secs(2).await;
+    Timer::after_secs(4).await;
 
     // Note that the LED pin on the Pico W is PIN_16
     let mut led = Output::new(p.PIN_25, Level::Low);
@@ -46,7 +65,9 @@ async fn main(spawner: Spawner) {
     let p27 = adc::Channel::new_pin(p.PIN_27, Pull::None);
     let p28 = adc::Channel::new_pin(p.PIN_28, Pull::None);
 
-    let Pio { mut common, sm0, .. } = Pio::new(p.PIO0, Irqs);
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(p.PIO0, Irqs);
     let pwm = PwmPio::attach(&mut common, sm0, p.PIN_1);
     let mut servo = ServoBuilder::new(pwm)
         .set_max_degree_rotation(120)
@@ -57,23 +78,27 @@ async fn main(spawner: Spawner) {
 
     // spawn the task that reads the ADC value
     spawner
-        .spawn(read_adc_value(adc, p26, p27, p28, CHANNEL_AMPLITUDES.sender()))
+        .spawn(read_adc_value(
+            adc,
+            p26,
+            p27,
+            p28,
+            CHANNEL_AMPLITUDES.sender(),
+        ))
         .unwrap();
 
     let rx_adv_value = CHANNEL_AMPLITUDES.receiver();
     led.set_high();
 
-    let mut level = 0;
+    info!("Getting started");
+
+    // let mut level = 0;
     loop {
-        level = (level + 1) % 120;
-        info!("level: {}", level);
-        servo.rotate(level);
-        Timer::after_millis(100).await;
-        // let amplitudes = rx_adv_value.receive().await;
-        // info!("NewData");
-        // for amplitude in amplitudes.iter() {
-        //     info!("{:?}", amplitude);
-        // }
-        // info!("EndData");
+        let amplitudes = rx_adv_value.receive().await;
+        info!("NewData");
+        for amplitude in amplitudes.iter() {
+            info!("{:?}", amplitude);
+        }
+        info!("EndData");
     }
 }
