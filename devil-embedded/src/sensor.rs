e03d7055983ab6f99eb6c4f9189ce23629c46947
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-09-12 18:20:12 -0700

    Added filter

--- devil-embedded/src/sensor.rs
+++ devil-embedded/src/sensor.rs
@@ -20,6 +20,10 @@ pub async fn read_adc_value(
     mut p28: adc::Channel<'static>,
     tx_value: Sender<'static, ThreadModeRawMutex, Amplitudes, 64>,
 ) {
+    let mut filter_1 = emg_filter_rs::EMGFilters::new(emg_filter_rs::SampleFrequency::Hz1000, emg_filter_rs::NotchFrequency::Hz60, true, true, true);
+    let mut filter_2 = emg_filter_rs::EMGFilters::new(emg_filter_rs::SampleFrequency::Hz1000, emg_filter_rs::NotchFrequency::Hz60, true, true, true);
+    let mut filter_3 = emg_filter_rs::EMGFilters::new(emg_filter_rs::SampleFrequency::Hz1000, emg_filter_rs::NotchFrequency::Hz60, true, true, true);
+
     let mut measurements_1 = [0f32; NUM_OF_MEASUREMENTS];
     let mut measurements_2 = [0f32; NUM_OF_MEASUREMENTS];
     let mut measurements_3 = [0f32; NUM_OF_MEASUREMENTS];
@@ -27,9 +31,9 @@ pub async fn read_adc_value(
     let mut amplitudes = [0u32; NUM_OF_MEASUREMENTS * 3];
     let mut pos = 0;
     loop {
-        measurements_1[pos] = adc.read(&mut p26).await.unwrap().into();
-        measurements_2[pos] = adc.read(&mut p27).await.unwrap().into();
-        measurements_3[pos] = adc.read(&mut p28).await.unwrap().into();
+        measurements_1[pos] = filter_1.update(adc.read(&mut p26).await.unwrap().into());
+        measurements_2[pos] = filter_2.update(adc.read(&mut p26).await.unwrap().into());
+        measurements_3[pos] = filter_3.update(adc.read(&mut p26).await.unwrap().into());
 
         pos = (pos + 1) % NUM_OF_MEASUREMENTS;
         if pos == 0 {
--- devil-embedded/src/servo.rs
+++ devil-embedded/src/servo.rs
@@ -1,6 +1,8 @@
 // This code has now been included in the raspberry pi pico embassy examples.
 use core::time::Duration;
+use easer::functions::Easing;
 use embassy_rp::pio::Instance;
+use embassy_time::Timer;
 use crate::pio_pwm::PwmPio;
 
 // The following will be interpreted as micro seconds.
@@ -28,7 +30,7 @@ impl<'d, T: Instance, const SM: usize> ServoBuilder<'d, T, SM> {
             period: Duration::from_micros(REFRESH_INTERVAL),
             min_pulse_width: Duration::from_micros(DEFAULT_MIN_PULSE_WIDTH),
             max_pulse_width: Duration::from_micros(DEFAULT_MAX_PULSE_WIDTH),
-            max_degree_rotation: DEFAULT_MAX_DEGREE_ROTATION
+            max_degree_rotation: DEFAULT_MAX_DEGREE_ROTATION,
         }
     }
 
@@ -68,6 +70,7 @@ impl<'d, T: Instance, const SM: usize> ServoBuilder<'d, T, SM> {
             min_pulse_width: self.min_pulse_width,
             max_pulse_width: self.max_pulse_width,
             max_degree_rotation: self.max_degree_rotation,
+            last_time_written: self.min_pulse_width.clone()
         }
     }
 }
@@ -77,6 +80,11 @@ pub struct Servo<'d, T: Instance, const SM: usize> {
     min_pulse_width: Duration,
     max_pulse_width: Duration,
     pub max_degree_rotation: u64,
+    last_time_written: Duration
+}
+
+pub enum EasingFunction {
+    Cubic
 }
 
 impl<'d, T: Instance, const SM: usize> Servo<'d, T, SM> {
@@ -92,18 +100,49 @@ impl<'d, T: Instance, const SM: usize> Servo<'d, T, SM> {
 
     // Write the duration that the Servo PWM cycle should run. More can be found from pio_pwm.rs
     pub fn write_time(&mut self, duration: Duration) {
+        self.last_time_written = duration.clone();
         self.pwm.write(duration);
     }
 
     // Rotate the servo to the specified degree.
     pub fn rotate(&mut self, degree: u64) {
+        let duration = self.degrees_to_time(degree);
+
+        self.pwm.write(duration);
+    }
+
+    fn degrees_to_time(&self, degree: u64) -> Duration {
         let degree_per_nano_second = (self.max_pulse_width.as_nanos() as u64 - self.min_pulse_width.as_nanos() as u64) / self.max_degree_rotation;
         let mut duration = Duration::from_nanos(degree * degree_per_nano_second + self.min_pulse_width.as_nanos() as u64);
         if self.max_pulse_width < duration {
             duration = self.max_pulse_width;
         }
 
-        self.pwm.write(duration);
+        duration
+    }
+
+    fn time_to_degree(&self, time: Duration) -> u64 {
+        let degree_per_nano_second = (self.max_pulse_width.as_nanos() as u64 - self.min_pulse_width.as_nanos() as u64) / self.max_degree_rotation;
+        let degree = (time - self.min_pulse_width).as_nanos() / degree_per_nano_second as u128;
+
+        degree as u64
+    }
+
+    // Ease moving into the specified degree
+    // tick is the amount of time between each update, and duration is the length of time to run easing funciton
+    // over with the specified easing function.
+    pub async fn ease_to(&mut self, degree: u64, refresh_rate: embassy_time::Duration, duration: embassy_time::Duration, easing_function: EasingFunction) {
+        let mut steps: f32 = 0.0;
+        let time_to_wite = self.degrees_to_time(degree);
+        let steps_end = (duration.as_ticks() / refresh_rate.as_ticks()) as f32;
+        loop {
+            steps += 1.0;
+            Timer::after(refresh_rate).await;
+
+            // match easing_function {
+                // EasingFunction::Cubic => easer::functions::Cubic::ease_in(steps, self.last_time_written.as_nanos() as f32, time_to_wite.as_nanos() as f32, steps_end),
+            // }
+        }
     }
 
     // Release the pwm struct from the servo
